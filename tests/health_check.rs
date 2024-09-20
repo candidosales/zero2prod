9382ab179dbd1468cb057f533b6f3c46e8f3ee15
Candido Sales Gomes <candidosg@gmail.com>
2024-09-20 07:39:41 -0400

    fix unit tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,14 @@
+use std::{fmt::format, net::TcpListener};
+
 use zero2prod;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -17,7 +18,14 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failted to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failted to bind address");
     let _ = tokio::spawn(server);
+
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
