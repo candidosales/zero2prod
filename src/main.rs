aa660ff2db038b1aec6c6fac8d82358764314689
Candido Sales Gomes <candidosg@gmail.com>
2024-09-20 07:22:44 -0400

    fix tests

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello Word")
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
-}
-
-#[cfg(test]]
-mod tests {
-    use crate::health_check;
-
-    #[tokio::test]
-    async fn health_check_succeeds() {
-        let response = health_check().await;
-        assert!(response.status().is_success())
-    }
+    run()?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,23 @@
-use zero2prod::main;
+use zero2prod;
 
-#[test]
-fn dummy_test() {
-    main()
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failted to bind address");
+    let _ = tokio::spawn(server);
 }
